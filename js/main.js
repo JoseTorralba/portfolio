57c2655871515be605f4429886688398905b0feb
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2019-07-03 16:42:15 -0400

    changed nav on desktop, design changes for about and contact, removed jQuery.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Document on Ready
 document.addEventListener('DOMContentLoaded', function() {
+
     // NAVIGATION OBJECT & VARIABLES
     var toggleItem = document.querySelectorAll('.navigation__item');
     var navigation = {
@@ -9,59 +10,62 @@ document.addEventListener('DOMContentLoaded', function() {
         background: document.querySelector('.navigation__background'),
         icon: document.querySelector('.navigation__toggle--icon')
     };
-  
+
     // When Mobile Nav is Clicked
     navigation.icon.addEventListener('click', function() {
         navigation.nav.classList.toggle('navigation__active');
-        
-        // Toggles Nav Icon
+        navigation.list.style.opacity = '1';
+        toggleNavIcon();
+
+        toggleItem.forEach(function (toggleItem) {
+            toggleItem.addEventListener('click', function() {
+                navigation.nav.classList.remove('navigation__active');
+                toggleNavIcon();
+            })
+        });
+    });
+    
+    // Toggles Nav Icon
+    function toggleNavIcon() {
         if (navigation.icon.classList.contains('fa-bars')) {
             navigation.icon.setAttribute("class", "fas fa-times navigation__toggle--icon");
             navigation.icon.removeAttribute("fas fa-bars navigation__toggle--icon");
-            navigation.list.style.opacity = '1';
-  
+
         } else {
             navigation.icon.setAttribute("class", "fas fa-bars navigation__toggle--icon");
             navigation.icon.removeAttribute('fas fa-times navigation__toggle--icon');
-            navigation.list.style.opacity = '0';
         }
-    });
-  
-    toggleItem.forEach(function (toggleItem) {
-        toggleItem.addEventListener('click', function() {
-            navigation.nav.classList.remove('navigation__active');
-  
-            // Toggles Nav Icon
-            if (navigation.icon.classList.contains('fa-bars')) {
-                navigation.icon.setAttribute("class", "fas fa-times navigation__toggle--icon");
-                navigation.icon.removeAttribute("fas fa-bars navigation__toggle--icon");
-    
-            } else {
-                navigation.icon.setAttribute("class", "fas fa-bars navigation__toggle--icon");
-                navigation.icon.removeAttribute('fas fa-times navigation__toggle--icon');
-            }
-        })
-    });
-    
-    // About & Header Background Image On Scroll
-    $(window).scroll(function() {
-        const scrollAboutPosition = $(window).scrollTop()/4;
-        $('.about-section').css({
-            'background-position-x' : + scrollAboutPosition + 'px'
-        });
+    }
 
-        const scrollHeaderPosition = $(window).scrollTop()/ 2;
-        $('.header').css({
-            'background-position-y' : + scrollHeaderPosition + 'px'
-        });
-    });
 
-    // Scroll animation
-    $("a.scroll").click(function (event) {
-        event.preventDefault();
+    // Header on Scroll Background Image
+    window.addEventListener('scroll', function() {
 
-        $("html, body").animate({
-            scrollTop: $($(this).attr("href")).offset().top
-        }, 1000);
-    });
+        // Navigation changes past header
+        var getNav = document.querySelector('.navigation__nav');
+        var getNavList = document.querySelector('.navigation__list');
+        
+        if (window.pageYOffset > 700) {
+            getNav.style.backgroundColor = '#000';
+            getNavList.style.padding = '1rem 1.5rem';
+
+        } else {
+            getNav.style.removeProperty("background-color");
+            getNavList.style.removeProperty("padding");
+
+        }
+
+        // Header Background on Scroll
+        const headerScroll = window.pageYOffset/2;
+        var getHeaderBackground = document.querySelector('.header');
+        getHeaderBackground.style.backgroundPositionY = headerScroll + 'px';
+
+        // About Background on Scroll
+        const aboutScroll = window.pageYOffset/2;
+        var getAboutBackground = document.querySelector('.about-section');
+        getAboutBackground.style.backgroundPositionX = aboutScroll + 'px';
+
+        // Contact Background gradient/color on scroll?
+
+    })
 });
