684f73d57e0e931cc9ac8fc8d3fdfa39c3e81441
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2022-09-11 16:12:27 -0400

    started on reworking my portfoliio

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const HeaderContainer = styled.div`
+  background-color: #000;
+  color: #fff;
+  height: 100vh;
+`;
+
+const Header = () => {
+  return <HeaderContainer>Header</HeaderContainer>;
+};
+
+export default Header;
